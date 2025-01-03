c1e96efbd88e361d0c0e807ab60d819906d817c0
cuplyk <121808859+cuplyk@users.noreply.github.com>
2025-01-03 22:54:48 +0000

    clean template

--- a_posts/views.py
+++ a_posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def home_view(request):
     title = 'welcome to django'
-    return render(request, 'index.html', {'title' : title})
+    return render(request, 'a_posts/home.html', {'title' : title})
